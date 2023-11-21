fc5f99ca791c2a4e8d93a1a25404d1d3abf1134b
Parsa <parsa.habibi.2002@gmail.com>
2023-11-21 18:17:52 +0330

    Refactor: fixed News page after Change Language

--- my-app/src/pages/News.jsx
+++ my-app/src/pages/News.jsx
@@ -7,20 +7,29 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import Cards from "../components/Card/Cards";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import {useLanguage} from "../LanguageContext";
+import {useIntl} from "react-intl";
 
 const News = () => {
+
+
+    const {locale, changeLocale} = useLanguage();
+
+    console.log(locale)
+
+    const intl = useIntl();
 
     const Airplane = '/assets/Images/Airplane.svg'
 
     const Abrisham = '/assets/Images/Abrisham.svg'
 
     const data = [
-        {id: 1, title: 'پیشنهاد واردات هواپیماهای با عمر بیش از ۲۰ سال به ایران', image: Airplane, des: ''},
-        {id: 2, title: 'نشست ملی راهبردهای مبادلات اقتصادی در مسیر جاده ابریشم', image: Abrisham, des: ''},
-        {id: 3, title: 'پیشنهاد واردات هواپیماهای با عمر بیش از ۲۰ سال به ایران', image: Airplane, des: ''},
-        {id: 4, title: 'نشست ملی راهبردهای مبادلات اقتصادی در مسیر جاده ابریشم', image: Abrisham, des: ''},
-        {id: 5, title: 'پیشنهاد واردات هواپیماهای با عمر بیش از ۲۰ سال به ایران', image: Airplane, des: ''},
-        {id: 6, title: 'نشست ملی راهبردهای مبادلات اقتصادی در مسیر جاده ابریشم', image: Abrisham, des: ''},
+        {id: 1, title: intl.$t({id: "TitleInformationCardNewsAirPlane"}), image: Airplane, des: ''},
+        {id: 2, title:intl.$t({id: "TitleInformationCardNewsRoad"}), image: Abrisham, des: ''},
+        {id: 3, title:intl.$t({id: "TitleInformationCardNewsAirPlane"}), image: Airplane, des: ''},
+        {id: 4, title:intl.$t({id: "TitleInformationCardNewsRoad"}), image: Abrisham, des: ''},
+        {id: 5, title:intl.$t({id: "TitleInformationCardNewsAirPlane"}), image: Airplane, des: ''},
+        {id: 6, title:intl.$t({id: "TitleInformationCardNewsRoad"}), image: Abrisham, des: ''},
     ]
 
     let variant = 'subtitle2'
@@ -38,24 +47,24 @@ const News = () => {
     }
 
     return (
-        <Grid>
+        <Grid dir={locale === 'fa' ? 'rtl' : 'ltr'}>
             <Header MobileId={4} DesktopId={4}/>
             <Grid mt={{xs: '88px', md: '192px'}} px={{xs: '20px', md: '120px', lg: '180px'}}>
-                <Typography variant={variant} fontWeight={900} pb={{xs: '16px', md: '40px'}} pt={{xs: '32px', md: ''}}>اخبار
-                    گروه آلتن</Typography>
+                <Typography variant={variant} fontWeight={900} pb={{xs: '16px', md: '40px'}} pt={{xs: '32px', md: ''}}>{intl.$t({id: "TitleNewsPage"})}</Typography>
                 <Grid display={'flex'} flexWrap={'wrap'} alignItems={'center'} justifyContent={'space-between'}
                       gap={{xs: '24px', md: '56px'}} pb={{xs: '32px', md: '42px'}}>
                     {
                         data.map(
                             item =>
-                                <Cards images={item.image} title={item.title} des={item.des}
+                                <Cards images={item.image} title={item.title}
                                        width={{xs: '128px', xxs: '152px', md: '300px'}}
                                        height={{xs: '212px', md: '420px'}} state={{xs: true, md: false}}
-                                       font={{xs: 500, md: 900}} variants={'h3'}/>
+                                       font={{xs: 500, md: 900}} variants={'h3'}
+                                />
                         )
                     }
                 </Grid>
-                <Grid display={'flex'} alignItems={'center'} justifyContent={'center'} gap={{xs:'10px' , md:'20px'}} pb={{xs:'32px' , md:'40px'}}>
+                <Grid display={'flex'} alignItems={'center'} justifyContent={'center'} gap={{xs:'10px' , md:'20px'}} pb={{xs:'32px' , md:'40px'}} dir={'rtl'}>
                     <ArrowForwardIosIcon fontSize={'large'} style={{cursor:'pointer'}}/>
                     <ArrowBackIosNewIcon fontSize={'large'} style={{cursor:'pointer'}}/>
                 </Grid>
